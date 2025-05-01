b2d0c472dccef943fa28af8c72ada57cecac6eae
Myrella Gomes <myrellagomes832@gmail.com>
2025-05-01 18:26:58 -0300

    atualizando calc.py

--- Calc.py
+++ Calc.py
@@ -1,3 +1,8 @@
+
+print ("---------------------")
+print ("Multiplicação de 10 por 2")
+print ("---------------------")
+
 for i in range (0, 11, 2):
     resultado = 10 * i
     print(f"10 x {i} = {resultado}") 
