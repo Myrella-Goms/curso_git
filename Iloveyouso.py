a20bc86f5e30a1715c532455c26dc654567b5fed
Myrella Gomes <myrellagomes832@gmail.com>
2025-05-01 17:12:18 -0300

    Alteração na Ilys.py

---
+++ Iloveyouso.py
@@ -0,0 +1,8 @@
+print("IMESSAGE:" "you have a new message from ayme!")
+print("-------------------------------------------")
+answer = (input("I love you so, do you love me too?: "))
+
+if answer in ["Yes", "I love you too"]:
+    print ("Thank you, honey, xoxo")
+else:
+    print ("Oh, I see...but I love you!")
